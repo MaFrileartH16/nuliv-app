63787d879235e7dcfe1d6686911beca125f4dc39
MaFrileartH16 <fauzul.hanif@mafrilearth.com>
2025-12-14 11:25:17 +0700

    chore: init eas android credentials

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,9 @@
-import { Button, H2, Paragraph, YStack } from 'tamagui'
+import { Button, H2, YStack } from 'tamagui'
 
 export default function HomeScreen() {
   return (
     <YStack flex={1} justify="center" items="center" gap="$4" px="$5" bg="$background">
       <H2>Home</H2>
-      <Paragraph opacity={0.7}>Ini satu-satunya halaman di aplikasi.</Paragraph>
-
       <Button onPress={() => {}}>Tombol Tamagui</Button>
     </YStack>
   )
